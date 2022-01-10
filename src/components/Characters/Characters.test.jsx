e968bb956e1ce315b829727f7028dcd954667e60
Michael Rider <mikepdxrider@gmail.com>
2022-01-10 12:00:05 -0800

    add character snapshot test

---
+++ src/components/Characters/Characters.test.jsx
@@ -0,0 +1,20 @@
+import {render, screen} from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom';
+import Characters from './Characters.jsx'
+
+it('characters details snapshot', async () => {
+    const { container } = render(
+        <MemoryRouter>
+            <Characters 
+            characters={[{
+                image: 'test-image',
+                name: 'test-name',
+                species: 'test-species',
+                status: 'test-status'
+            }]}
+            />
+        </MemoryRouter>
+    );
+
+    expect(container).toMatchSnapshot();
+})
