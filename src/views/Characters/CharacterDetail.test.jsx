8b2c0a38ad4d02660e9958496dc92dd52583ae6f
Michael Rider <mikepdxrider@gmail.com>
2022-01-10 13:20:14 -0800

    adds mock API to characters behavior tests

--- src/views/Characters/CharacterDetail.test.jsx
+++ src/views/Characters/CharacterDetail.test.jsx
@@ -2,9 +2,48 @@ import {render, screen} from '@testing-library/react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import CharacterDetail from './CharacterDetail.jsx'
 import CharacterList from './CharacterList.jsx'
-// import { rest } from 'msw';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const server = setupServer(
+    rest.get('https://rickandmortyapi.com/api/character/:id', (req, res, ctx) => {
+        return res(
+            ctx.json(
+                {
+                    image: 'test-image',
+                    name: 'test-name',
+                    species: 'test-species',
+                    status: 'test-status'
+                }
+            )
+        )
+    }),
+    rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
+        return res(
+            ctx.json({
+                info: { pages: 1},
+                results: [
+                    {
+                        image: 'test-image',
+                        name: 'test-name',
+                        species: 'test-species',
+                        status: 'test-status'
+                    }
+                ]
+            })
+        )
+    })
+);
 
 describe('Characters behavior', () => {
+    beforeAll(
+        () => server.listen()
+    );
+
+    afterAll(
+        () => server.close()
+    );
+
     it('renders correct character details', async () => {
         render(
             <MemoryRouter initialEntries={['/characters/1']}>
@@ -15,7 +54,8 @@ describe('Characters behavior', () => {
         )
 
         screen.getByText('Loading character...');
-        await screen.findByText('Rick Sanchez');
+
+        await screen.findByText('test-name');
     })
 
     it('renders a list of characters', async () => {
